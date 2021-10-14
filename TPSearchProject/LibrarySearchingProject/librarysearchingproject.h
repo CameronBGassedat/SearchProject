0a07c6f4de16803c094fa216aef56a87a6454ae7
Alexis Brouard <67193389+alexisbrouard@users.noreply.github.com>
2021-10-14 13:48:56 +0200

    base

---
+++ TPSearchProject/LibrarySearchingProject/librarysearchingproject.h
@@ -0,0 +1,12 @@
+#ifndef LIBRARYSEARCHINGPROJECT_H
+#define LIBRARYSEARCHINGPROJECT_H
+
+#include "LibrarySearchingProject_global.h"
+
+class LIBRARYSEARCHINGPROJECT_EXPORT LibrarySearchingProject
+{
+public:
+    LibrarySearchingProject();
+};
+
+#endif // LIBRARYSEARCHINGPROJECT_H
