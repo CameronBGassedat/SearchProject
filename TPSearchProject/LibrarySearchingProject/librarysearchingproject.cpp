aa59bb9088c97307999318e55342c41ad8aea6b8
Alexis Brouard <67193389+alexisbrouard@users.noreply.github.com>
2021-10-14 14:28:00 +0200

    add working library

--- TPSearchProject/LibrarySearchingProject/librarysearchingproject.cpp
+++ TPSearchProject/LibrarySearchingProject/librarysearchingproject.cpp
@@ -1,5 +1,46 @@
 #include "librarysearchingproject.h"
 
-LibrarySearchingProject::LibrarySearchingProject()
+#include <QDebug>
+#include <QDirIterator>
+
+LibrarySearchingProject::LibrarySearchingProject() : _currentProgress(0), _maxProgress(0)
 {
+}
+
+int LibrarySearchingProject::maxProgress() const {
+    return _maxProgress;
+}
+
+int LibrarySearchingProject::currentProgress() const {
+    return _currentProgress;
+}
+
+QStringList LibrarySearchingProject::results() const {
+    return m_results;
+}
+
+void LibrarySearchingProject::setFilters(const QString &filters) {
+    m_filters = filters;
+}
+
+void LibrarySearchingProject::setStartDirectory(const QString &startDirectory) {
+    m_startDirectory = startDirectory;
+}
+
+bool LibrarySearchingProject::isAvaiable() const {
+    return _isAvaiable;
+}
+
+void LibrarySearchingProject::start() {
+    qDebug() << __FUNCTION__ << __LINE__ << "Name of top directory: " << m_startDirectory << m_filters;
+
+    QDirIterator it(m_startDirectory, m_filters.split(","), QDir::Files, QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        // qDebug() << it.next();
+        m_results << it.next();
+        _currentProgress++;
+    }
+}
+
+void LibrarySearchingProject::stop() {
 }
--- TPSearchProject/LibrarySearchingProject/librarysearchingproject.h
+++ TPSearchProject/LibrarySearchingProject/librarysearchingproject.h
@@ -2,11 +2,29 @@
 #define LIBRARYSEARCHINGPROJECT_H
 
 #include "LibrarySearchingProject_global.h"
+#include <QString>
+#include <QStringList>
 
 class LIBRARYSEARCHINGPROJECT_EXPORT LibrarySearchingProject
 {
+    QString     m_startDirectory;
+    QString     m_filters;
+    QStringList m_results;
+    int         _currentProgress;
+    int         _maxProgress;
+    bool        _isAvaiable;
+
 public:
     LibrarySearchingProject();
+    void        start();
+    void        stop();
+    int         maxProgress() const;
+    int         currentProgress() const;
+    QStringList results() const;
+    void        setFilters(const QString &filters);
+    void        setStartDirectory(const QString &startDirectory);
+    bool        isAvaiable() const;
+
 };
 
 #endif // LIBRARYSEARCHINGPROJECT_H
